42d7d182a1bc042f397305a00aad0323565c795e
Giacomo <g.lanciano1@gmail.com>
2016-10-23 18:18:20 +0200

    improve shell script to check download result

--- 01/check-recipes.sh
+++ 01/check-recipes.sh
@@ -8,21 +8,23 @@
 #######################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SRC="$DIR/recipes-names.txt"
-DEST="$DIR/recipes_old"
+SRC="$DIR/recipes.txt"
+#DEST="$DIR/recipes"
+DEST="$DIR/recipes_test"
+URL_PREFIX_LENGTH=34
 TIMEOUT="1"
 
 i=0
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    name=$(echo ${line} | tr -d '\r')
+    name=$(echo ${line:URL_PREFIX_LENGTH} | tr -d '\r')
     
     if [ ! -f "$DEST/$name.html" ]; then
-        printf "%s\n" $name
+        printf "%s\n" $(echo ${line} | tr -d '\r')
         ((i += 1))
     fi
-    #if [ $i = 10 ]; then
-    #    exit 1
-    #fi
+    if [ $i = 10 ]; then
+        exit 1
+    fi
 done < $SRC
 
 printf "\n%i recipes not downloaded.\n" $i
