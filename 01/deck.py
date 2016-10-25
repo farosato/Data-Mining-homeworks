2a54352d127db6a793c6a3237e7aeaa4c7af4a4b
Giacomo <g.lanciano1@gmail.com>
2016-10-25 21:36:16 +0200

    refactoring

--- 01/deck.py
+++ 01/deck.py
@@ -1,8 +1,8 @@
 from __future__ import division
-import pydealer
 import time
+import pydealer
 
-TRIALS = 1000000    # ~3 mins for a million trials
+TRIALS = 1000000  # ~3 mins for a million trials
 FORMAT = '{0:.5f}'
 
 atLeast1Ace3Cards = 0
@@ -11,47 +11,54 @@ exactly1Ace5Cards = 0
 allDiamonds5Cards = 0
 full5Cards = 0
 
-def at_least_1_ace_3_cards(hand):
-    return any(c.value == 'Ace' for c in hand[:3])
 
-def all_equals_3_cards(hand):
-    return hand[0].value == hand[1].value and hand[1].value == hand[2].value\
-            and hand[0].value == hand[2].value
+def at_least_1_ace_3_cards(cards):
+    return any(c.value == 'Ace' for c in cards[:3])
 
-def exactly_1_ace_5_cards(hand):
-    aceFound = False
-    for c in hand:
-        if c.value == 'Ace' and not aceFound:
-            aceFound = True
-        elif c.value == 'Ace' and aceFound:
+
+def all_equals_3_cards(cards):
+    return cards[0].value == cards[1].value and cards[1].value == cards[2].value \
+           and cards[0].value == cards[2].value
+
+
+def exactly_1_ace_5_cards(cards):
+    ace_found = False
+    for c in cards:
+        if c.value == 'Ace' and not ace_found:
+            ace_found = True
+        elif c.value == 'Ace' and ace_found:
             return False
-    return aceFound
+    return ace_found
 
-def all_diamonds_5_cards(hand):
-    for c in hand:
+
+def all_diamonds_5_cards(cards):
+    for c in cards:
         if c.suit != 'Diamonds':
             return False
     return True
 
-def full_5_cards(hand):
+
+def full_5_cards(cards):
     # NOTE: hand is passed by reference, sort() update original
-    hand.sort()
-    return (hand[0].value == hand[1].value == hand[2].value and hand[3].value == hand[4].value\
-                and hand[0].value != hand[3].value)\
-            or (hand[0].value == hand[1].value and hand[2].value == hand[3].value == hand[4].value\
-                and hand[0].value != hand[2].value)
+    cards.sort()
+    return (cards[0].value == cards[1].value == cards[2].value and cards[3].value == cards[4].value \
+            and cards[0].value != cards[3].value) \
+           or (cards[0].value == cards[1].value and cards[2].value == cards[3].value == cards[4].value \
+               and cards[0].value != cards[2].value)
+
 
 def print_results():
-    print '##### Frequencies report on %d trials #####\n' % (TRIALS) 
+    print '##### Frequencies report on %d trials #####\n' % TRIALS
     print 'At least 1 Ace in first 3 cards:\t' + FORMAT.format(atLeast1Ace3Cards / TRIALS)
     print 'First 3 cards all equals:\t\t' + FORMAT.format(allEquals3Cards / TRIALS)
     print 'Exactly 1 Ace in first 5 cards:\t\t' + FORMAT.format(exactly1Ace5Cards / TRIALS)
     print 'All Diamonds in first 5 cards:\t\t' + FORMAT.format(allDiamonds5Cards / TRIALS)
     print 'Full in first 5 cards:\t\t\t' + FORMAT.format(full5Cards / TRIALS)
 
+
 if __name__ == "__main__":
     start_time = time.time()
-    for i in range(0,TRIALS):
+    for i in range(0, TRIALS):
         deck = pydealer.Deck()
         deck.shuffle()
         hand = deck.deal(5)
@@ -67,4 +74,3 @@ if __name__ == "__main__":
             full5Cards += 1
     print_results()
     print("\n--- %s seconds ---" % (time.time() - start_time))
-
--- 01/preprocess-recipes.py
+++ 01/preprocess-recipes.py
@@ -1,61 +1,60 @@
-import nltk
 import os.path
+from os import listdir
+import nltk
 import unicodecsv as csv
 from bs4 import BeautifulSoup
-from os import listdir
 
-#SRC = './recipes'
+# SRC = './recipes'
 SRC = './recipes_test'
 DEST = './recipes.tsv'
 DEST_PREP = './recipes-prep.tsv'
-HEADERS_LINE = ['title','author','prep_time','cook_time','num_people','dietary_info','ingredients','method']
+HEADERS_LINE = ['title', 'author', 'prep_time', 'cook_time', 'num_people', 'dietary_info', 'ingredients', 'method']
 
 # parse recipes and put data in a single .tsv file
 with open(DEST, 'wb') as out:
     tsvWriter = csv.writer(out, delimiter='\t')
     tsvWriter.writerow(HEADERS_LINE)
-    
+
     for f in listdir(SRC):
         print f
         row = []
         fid = open(os.path.join(SRC, f), 'r')
         recipeSoup = BeautifulSoup(fid, 'html.parser')
-        
+
         for title in recipeSoup.find_all('h1', class_='content-title__text'):
             row.append(title.text.strip())
-        
+
         for author in recipeSoup.find_all('a', class_='chef__link', itemprop='author'):
             row.append(author.text.strip())
-        
+
         for prep_time in recipeSoup.find_all('p', class_='recipe-metadata__prep-time'):
             row.append(prep_time.text.strip())
-        
+
         for cook_time in recipeSoup.find_all('p', class_='recipe-metadata__cook-time'):
             row.append(cook_time.text.strip())
-        
+
         for serving in recipeSoup.find_all('p', class_='recipe-metadata__serving'):
             row.append(serving.text.strip())
-        
+
         dietary = recipeSoup.find_all('p', class_='recipe-metadata__dietary-vegetarian-text')
         for d in dietary:
             row.append(d.text.strip())
         else:
             if len(dietary) == 0:
                 row.append('')
-        
+
         ingredients = []
         for i in recipeSoup.find_all('li', class_='recipe-ingredients__list-item'):
             ingredients.append(i.text.strip())
         row.append(" | ".join(ingredients))
-        
+
         method = []
         for m in recipeSoup.find_all('p', class_='recipe-method__list-item-text'):
             method.append(m.text.strip())
         row.append(" ".join(method))
-        
+
         tsvWriter.writerow(row)
         fid.close()
-
 
 # pre-processing
 stops = set(nltk.corpus.stopwords.words('english'))
@@ -64,30 +63,29 @@ stemmer = nltk.stem.porter.PorterStemmer()
 with open(DEST_PREP, 'wb') as out:
     tsvWriter = csv.writer(out, delimiter='\t')
     tsvWriter.writerow(HEADERS_LINE)
-    
+
     tsv = open(DEST, 'rb')
     tsvReader = csv.reader(tsv, delimiter='\t')
-    
+
     next(tsvReader)
     for row in tsvReader:
         outRow = []
-        
+
         # do not pre-process title and author
         outRow.append(row[0])
         outRow.append(row[1])
-        
+
         for field in row[2:]:
             # TODO: ask if sent_tokenize is necessary for 'method' field
             # tokenizaton & stopwords removal
             tokens = [word for word in nltk.word_tokenize(field) if word not in stops]
-            
+
             # stemming
             tokens = [stemmer.stem(t) for t in tokens]
-            
-            #print tokens
+
+            # print tokens
             outRow.append(" ".join(tokens))
-        
+
         tsvWriter.writerow(outRow)
-            
 
     tsv.close()
