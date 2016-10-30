b440b94a6fb9735ddb28b5cd55b15cd3a9ab6fa5
FanTempTime <f4b.r0s@gmail.com>
2016-10-30 19:45:48 +0100

    Refines non-alphanumeric tokens removal.

--- 01/preprocessing.py
+++ 01/preprocessing.py
@@ -6,9 +6,7 @@ Non-ASCII characters are used. Be sure to set utf-8 encoding in your text editor
 """
 import nltk
 from unidecode import unidecode
-
-HALF = u'½'
-HALF_REPLACEMENT = u'.5'
+import re
 
 stopwords = set(nltk.corpus.stopwords.words('english'))
 stemmer = nltk.stem.porter.PorterStemmer()
@@ -17,6 +15,7 @@ stemmer = nltk.stem.porter.PorterStemmer()
 def preprocess(text, stem=False):
     tokens = _tokenize(text)
     tokens = _normalize(tokens)
+    tokens = _remove_non_alphanum(tokens)
     tokens = _remove_stopwords(tokens)
     if stem: tokens = _stem(tokens)
     return tokens
@@ -27,13 +26,28 @@ def _tokenize(text):
 
 
 def _normalize(tokens):
-    """Normalize tokens by removing diacritics and converting to lowercase."""
-    tokens = _replace_half(tokens) # has to be called before unideconding
+    # unidecode removes diacritics and converts special unicode characters (like fractions) into a
+    # somewhat "equivalent" ASCII representation (e.g. splits u'½' in '1/2')
     return [unidecode(t.lower()) for t in tokens]
+    # tokens = _replace_fractions(tokens) # do we even care? disabled for now
+
+
+def _replace_fractions(tokens):
+    # return [t.replace(u'½', u'0.5') for t in tokens]
+    pass
 
 
 def _remove_non_alphanum(tokens):
-    return [t for t in tokens if t.isalnum()]
+    return [t for t in tokens if _isalnum(t)]
+
+
+def _isalnum(t):
+    if t.isalnum():
+        return True
+    elif re.match(r'[0-9]*[./][0-9]+'): # decimal number or fraction
+        return True;
+    else:
+        return False
 
 
 def _remove_stopwords(tokens):
@@ -42,8 +56,3 @@ def _remove_stopwords(tokens):
 
 def _stem(tokens):
     return [stemmer.stem(t) for t in tokens]
-
-
-def _replace_half(tokens):
-    """Replace non-ASCII 'half' character to preserve ingredients quantities"""
-    return [t.replace(HALF, HALF_REPLACEMENT) for t in tokens]
