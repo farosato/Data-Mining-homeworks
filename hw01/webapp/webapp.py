c0e257e18d4b395c9d27bf21494b6cf55c0290b0
Francisco FG <matakukos@gmail.com>
2016-11-11 21:53:39 +0100

    added functionality to get query
    edited the index function to get the query string

--- hw01/webapp/webapp.py
+++ hw01/webapp/webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request, redirect, url_for
 import sys
 import os.path
 
@@ -6,12 +6,22 @@ import os.path
 # Be careful that following line keeps on pointing to homework 1 root directory.
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import process_queries
+from query_processing import process_query
 
 app = Flask(__name__)
-
-@app.route('/')
+    
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    query = None
+    query=request.args.get("search_form_input_homepage")
+    if query!=None:
+        index = process_queries.load_index()
+        recipes=None
+        #process_queries.retrieve_docs_contents(process_queries.process_query(index, query))
+        #Funzioni per cercare e insertare le ricetti in recipes
+        return render_template('show_recipes.html', entries=entries)
     return send_file('index.html')
-
+    
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
