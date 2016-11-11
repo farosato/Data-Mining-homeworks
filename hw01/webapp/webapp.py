a1610bdc2803faf42d2434a1c36aa08c5b4e5250
FanTempTime <f4b.r0s@gmail.com>
2016-11-11 19:39:09 +0100

    Modifies index route.

--- hw01/webapp/webapp.py
+++ hw01/webapp/webapp.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template
-from hw01 import process_queries
+from flask import Flask, render_template, send_file
+# from hw01 import process_queries
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
