4f391bd6fc430f0d8786cab260f6e4320f4006ca
FanTempTime <f4b.r0s@gmail.com>
2016-11-12 17:02:12 +0100

    Completes webapp templates.

--- hw01/webapp/webapp.py
+++ hw01/webapp/webapp.py
@@ -5,7 +5,7 @@ import os.path
 # process_queries module has to be added manually to path before import. IDE may complain about it, just ignore it.
 # Be careful that following line keeps on pointing to homework 1 root directory.
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import process_queries
+import process_queries as pq
 
 app = Flask(__name__)
 
@@ -14,12 +14,12 @@ app = Flask(__name__)
 def index():
     query = request.args.get('query')
     if query:
-        results = process_queries.retrieve_docs_contents(process_queries.process_query(inv_index, str(query)), False)
+        results = pq.retrieve_docs_contents(pq.process_query(index, str(query)))
         return render_template('query_results.html', query=query, results=results)
     else:
         return send_file('index.html')
 
 
 if __name__ == '__main__':
-    inv_index = process_queries.load_index()
+    index = pq.load_index()
     app.run(debug=True, host='0.0.0.0')
