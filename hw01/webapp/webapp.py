20f423d1b5e95f3301be07413c1222c3df57797c
FanTempTime <f4b.r0s@gmail.com>
2016-11-13 13:38:54 +0100

    Refines query results display in the webapp.

--- hw01/webapp/webapp.py
+++ hw01/webapp/webapp.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template, send_file, request
 import sys
 import os.path
+import time
 
 # process_queries module has to be added manually to path before import. IDE may complain about it, just ignore it.
 # Be careful that following line keeps on pointing to homework 1 root directory.
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import process_queries as pq
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def root():
     query = request.args.get('query')
     if query:
+        start_time = time.time()
         results = pq.retrieve_docs_contents(pq.process_query(index, str(query)))
-        return render_template('query_results.html', query=query, results=results)
+        elapsed = time.time() - start_time
+        return render_template('query_results.html', query=query, time=elapsed, results=results)
     else:
         return send_file('index.html')
 
 
 if __name__ == '__main__':
     index = pq.load_index()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=False, host='0.0.0.0')  # set debug to True to allow auto-reloading during development
