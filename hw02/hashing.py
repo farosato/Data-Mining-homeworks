efcddac1de1caa1da053f874a8a66a7c0d082980
Giacomo <g.lanciano1@gmail.com>
2016-12-01 22:06:29 +0100

    add lsh on shingles

--- hw02/hashing.py
+++ hw02/hashing.py
@@ -1,15 +1,20 @@
 """ Module containing hashing functionalities. """
+from __future__ import division
 import hashlib
 
 
-def hash_family(i):
+JACCARD_THRESHOLD = 0.8
+
+
+def hash_family(i, result_size=8, max_length=20):
     """
     Implement a family of hash functions. It hashes strings and
     takes an integer to define the member of the family.
     Return a hash function parametrized by i.
+
+    :param result_size: how many bytes we want back
+    :param max_length: how long can our i be (in decimal)
     """
-    result_size = 8     # how many bytes we want back
-    max_length = 20     # how long can our i be (in decimal)
     salt = str(i).zfill(max_length)[-max_length:]
 
     def hash_member(x):
@@ -39,10 +44,42 @@ def lsh_shingles(docs_shingles):
     Given the shingles sets of each document in a set, finds the nearest neighbors
     by comparing all the shingle sets with each other.
     """
-    pass
+    neighbors = {}  # doc_id -> list of nearest neighbors doc_ids
+    corpus_size = len(docs_shingles)
+    for i, s_row in enumerate(docs_shingles):
+        for j in range(i, corpus_size):
+            if i != j and _jaccard(docs_shingles[i], docs_shingles[j]) >= JACCARD_THRESHOLD:
+                try:
+                    neighbors[i].append(j)
+                except KeyError:  # doc_id is not in dictionary keys
+                    neighbors[i] = [j]
+                try:
+                    neighbors[j].append(i)
+                except KeyError:  # doc_id is not in dictionary keys
+                    neighbors[j] = [i]
+    return neighbors
+
+
+def _jaccard(a, b):
+    """
+    Computes Jaccard similarity of the given sets.
+    Note that params are required to be Python sets.
+    """
+    return len(a.intersection(b)) / len(a.union(b))
 
 
 if __name__ == "__main__":
     # example of usage for hash_family()
-    hash_function = hash_family(2)
-    print hash_function('hello')
+    # hash_function = hash_family(2)
+    # print hash_function('hello')
+
+    # example of usage for lsh_shingles()
+    # import shingling
+    # docs = ['a rose is', 'a rose is', 'a rose is', 'a rose is', 'a rose is', 'a rose is']
+    # docs_shingles = [shingling.shingle_hash(d, 4) for d in docs]
+    # for ds in docs_shingles:
+    #     print ds
+    # for i in lsh_shingles(docs_shingles).items():
+    #     print i
+
+    pass
