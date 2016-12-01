619386c9e9e289adda6fad6d31d1e6efbff78b8d
Giacomo <g.lanciano1@gmail.com>
2016-12-01 21:29:40 +0100

    improve shingling

--- hw02/shingling.py
+++ hw02/shingling.py
@@ -8,20 +8,28 @@ HASH_ID = 2
 def shingle(doc, k):
     """
     Given a document, creates its set of character shingles of some length k.
-    Then represent the document as the set of the hashes of the shingles,
+    """
+    if len(doc) <= k:
+        shingles = [doc]
+    else:
+        shingles = [doc[i:i+k] for i in range(0, len(doc)-k+1)]
+    return set(shingles)
+
+
+def shingle_hash(doc, k):
+    """
+    Represent the document as the set of the hashes of the shingles,
     for some hash function.
     """
-    shingles = []
     hash_function = hashing.hash_family(HASH_ID)
-    if len(doc) <= k:
-        shingles.append(hash_function(doc))
-    else:
-        shingles = [hash_function(doc[i:i+k]) for i in range(0, len(doc)-k+1)]
-    return shingles
+    return set([hash_function(s) for s in shingle(doc, k)])
 
 
 if __name__ == "__main__":
     # example of usage for shingle()
     print shingle('a rose is a rose is a rose', 4)
+    print shingle_hash('a rose is a rose is a rose', 4)
     print shingle('a ros', 4)
+    print shingle_hash('a ros', 4)
     print shingle('a ro', 4)
+    print shingle_hash('a ro', 4)
