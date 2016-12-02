5b225d3575408d8b73fab1a2af09e4291f49cf1a
Giacomo <g.lanciano1@gmail.com>
2016-12-02 15:44:53 +0100

    refactoring

--- hw02/near_duplicates.py
+++ hw02/near_duplicates.py
@@ -11,7 +11,7 @@ import shingling
 JACCARD_THRESHOLD = 0.8
 
 
-def lsh_near_duplicates():
+def lsh_near_duplicates(docs_signatures):
     """
     Given a collection of minwise hash signatures of a set of documents,
     it finds the all the documents pairs that are near each other.
@@ -19,14 +19,15 @@ def lsh_near_duplicates():
 
     1. Pick a value of k and construct from each document the set of k-shingles.
     Optionally, hash the k-shingles to shorter bucket numbers.
-    [NOTE: this step is implemented in shingling.py]
+    [NOTE: this step is implemented in shingling.py. It is performed outside
+    this function to reuse shingles in brute force approach.]
 
     2. Sort the document-shingle pairs to order them by shingle.
 
     3. Pick a length n for the minhash signatures. Feed the sorted list to the
     algorithm of Section 3.3.5 to compute the minhash signatures for all the
     documents.
-    [NOTE: this step is implemented in hashing.minwise_hashing()]
+    [NOTE: this step is implemented in hashing.minwise_hashing().]
 
     4. Choose a threshold t that defines how similar documents have to be in
     order for them to be regarded as a desired "similar pair." Pick a number
@@ -35,10 +36,10 @@ def lsh_near_duplicates():
     you may wish to select b and r to produce a threshold lower than t; if
     speed is important and you wish to limit false positives, select b and r to
     produce a higher threshold.
-    [NOTE: t = 0.8, n = ]
+    [NOTE: t = 0.8, n = ...]
 
     5. Construct candidate pairs by applying the LSH technique of Section 3.4.1.
-    [NOTE: this step is implemented in hashing.lsh()]
+    [NOTE: this step is implemented in hashing.lsh().]
 
     6. Examine each candidate pair's signatures and determine whether the fraction of
     components in which they agree is at least t.
@@ -76,7 +77,7 @@ def brute_force_near_duplicates(docs_shingles):
 def _jaccard_sim(a, b):
     """
     Computes Jaccard similarity of the given sets.
-    Note that params are required to be Python sets.
+    Note that both params are required to be Python sets.
     """
     return len(a.intersection(b)) / len(a.union(b))
 
