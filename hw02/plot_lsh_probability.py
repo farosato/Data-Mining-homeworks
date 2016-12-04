384e3768cec7e592c2ae9edf3a5f445ae1719d7f
Giacomo <g.lanciano1@gmail.com>
2016-12-04 01:33:50 +0100

    add script to plot lsh probability function

---
+++ hw02/plot_lsh_probability.py
@@ -0,0 +1,18 @@
+import pylab
+import numpy
+import os
+from hashing import BANDS, ROWS_PER_BAND
+
+
+def lsh_probability(x):
+    return 1 - (1 - x**ROWS_PER_BAND)**BANDS
+
+
+if __name__ == "__main__":
+    x = numpy.linspace(0, 1)
+
+    # compose plot
+    pylab.plot(x, lsh_probability(x))
+    pylab.savefig(os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'report', 'img', 'lsh_probability.jpg'))
+    pylab.show()
