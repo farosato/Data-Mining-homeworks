3c6f5106dca27936c33e2674031cda494cdb78bd
Giacomo <g.lanciano1@gmail.com>
2016-12-04 11:13:28 +0100

    improve lsh probability function plotting

--- hw02/plot_lsh_probability.py
+++ hw02/plot_lsh_probability.py
@@ -13,6 +13,8 @@ if __name__ == "__main__":
 
     # compose plot
     pylab.plot(x, lsh_probability(x))
+    pylab.xlabel('Jaccard similarity')
+    pylab.ylabel('Probability of collision')
     pylab.savefig(os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), 'report', 'img', 'lsh_probability.jpg'))
+        os.path.dirname(os.path.abspath(__file__)), 'report', 'img', 'lsh_probability.jpg'), bbox_inches='tight')
     pylab.show()
